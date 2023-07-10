37ff849d0ab6987b75c29911a02df1733234baa1
Dmitry C <dmitry.cherkashin@genesys.com>
2023-07-10 20:20:34 +0200

    monkey asking

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,20 @@
-console.log('This would be the main JS file.');
+window.onload = function() {
+    new ClipboardJS('.cc-copy-btn');
+
+    const qrcode = new QRCode("qrcode");
+    document.getElementById('qrcode').onclick = function (el) {
+        qrcode.clear();
+        let img = el.currentTarget.querySelector('img');
+        img.style.display = 'none';
+    }
+    document.querySelectorAll('.cc-copy-btn').forEach(el => {
+        const target = el.getAttribute('data-clipboard-target');
+        el.onclick = function () {
+            const targetEl = document.querySelector(target);
+            qrcode.clear();
+            qrcode.makeCode(targetEl.innerText);
+        }
+    })
+}
+
+
